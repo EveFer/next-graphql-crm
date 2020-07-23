0c001cdcf430649bd8c899d1742a27adc46a7871
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2020-07-23 10:10:15 -0500

    [ADD] Implement for formik to handle forms

--- pages/signup.js
+++ pages/signup.js
@@ -1,21 +1,38 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 export default function Signup () {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      lastName: '',
+      email: '',
+      password: ''
+    },
+    onSubmit: values => {
+      console.log('enviando')
+      console.log(values)
+    }
+  })
   return (
     <>
       <Layout>
         <h1 className='text-center text-2xl text-white font-light'>Nueva Cuenta</h1>
         <div className='flex justify-center mt-5'>
           <div className='w-full max-w-sm'>
-            <form className='bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4'>
+            <form className='bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4' onSubmit={formik.handleSubmit}>
               <div className='mb-4'>
                 <label htmlFor='name' className='block text-gray-700 text-sm font-bold mb-2'>Nombre</label>
                 <input
                   className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
                   id='name'
                   type='text'
+                  name='name'
                   placeholder='Nombre Usuario'
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
                 />
               </div>
               <div className='mb-4'>
@@ -24,7 +41,10 @@ export default function Signup () {
                   className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
                   id='lastName'
                   type='text'
+                  name='lastName'
                   placeholder='Apellido Usuario'
+                  value={formik.values.lastName}
+                  onChange={formik.handleChange}
                 />
               </div>
               <div className='mb-4'>
@@ -33,7 +53,10 @@ export default function Signup () {
                   className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
                   id='email'
                   type='email'
+                  name='email'
                   placeholder='Email Usuario'
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
                 />
               </div>
               <div className='mb-4'>
@@ -42,7 +65,10 @@ export default function Signup () {
                   className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
                   id='password'
                   type='password'
+                  name='password'
                   placeholder='*****'
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
                 />
               </div>
               <input
