9f5d78d074ddaab714ed8b5ce89cf380a8b46d31
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2020-07-23 10:25:26 -0500

    [ADD] Implement validations to create account form

--- pages/signup.js
+++ pages/signup.js
@@ -11,6 +11,12 @@ export default function Signup () {
       email: '',
       password: ''
     },
+    validationSchema: Yup.object({
+      name: Yup.string().required('El nombre es Requerido'),
+      lastName: Yup.string().required('El Apellido es Requerido'),
+      email: Yup.string().email('Email no Valido').required('El email es Requerido'),
+      password: Yup.string().required('El Password es Requerido').min(8, 'El Password debe ser mayor de 8 caracteres')
+    }),
     onSubmit: values => {
       console.log('enviando')
       console.log(values)
@@ -33,8 +39,17 @@ export default function Signup () {
                   placeholder='Nombre Usuario'
                   value={formik.values.name}
                   onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
               </div>
+              {
+                formik.touched.name && formik.errors.name ? (
+                  <div className='my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-2'>
+                    <p className='font-bold'>Error</p>
+                    <p>{formik.errors.name}</p>
+                  </div>
+                ) : null
+              }
               <div className='mb-4'>
                 <label htmlFor='lastName' className='block text-gray-700 text-sm font-bold mb-2'>Apellido</label>
                 <input
@@ -45,8 +60,17 @@ export default function Signup () {
                   placeholder='Apellido Usuario'
                   value={formik.values.lastName}
                   onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
               </div>
+              {
+                formik.touched.lastName && formik.errors.lastName ? (
+                  <div className='my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-2'>
+                    <p className='font-bold'>Error</p>
+                    <p>{formik.errors.lastName}</p>
+                  </div>
+                ) : null
+              }
               <div className='mb-4'>
                 <label htmlFor='email' className='block text-gray-700 text-sm font-bold mb-2'>Email</label>
                 <input
@@ -57,8 +81,17 @@ export default function Signup () {
                   placeholder='Email Usuario'
                   value={formik.values.email}
                   onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
               </div>
+              {
+                formik.touched.email && formik.errors.email ? (
+                  <div className='my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-2'>
+                    <p className='font-bold'>Error</p>
+                    <p>{formik.errors.email}</p>
+                  </div>
+                ) : null
+              }
               <div className='mb-4'>
                 <label htmlFor='password' className='block text-gray-700 text-sm font-bold mb-2'>Password</label>
                 <input
@@ -69,8 +102,17 @@ export default function Signup () {
                   placeholder='*****'
                   value={formik.values.password}
                   onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
               </div>
+              {
+                formik.touched.password && formik.errors.password ? (
+                  <div className='my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-2'>
+                    <p className='font-bold'>Error</p>
+                    <p>{formik.errors.password}</p>
+                  </div>
+                ) : null
+              }
               <input
                 type='submit'
                 className='bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900'
